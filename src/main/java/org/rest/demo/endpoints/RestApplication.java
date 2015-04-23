09f6594b049308d477339da8c67067c5ff4331e8
s-ahmedouali <souhail.market@gmail.com>
2015-04-23 17:29:50 +0200

    init commit

---
+++ src/main/java/org/rest/demo/endpoints/RestApplication.java
@@ -0,0 +1,9 @@
+package org.rest.demo.endpoints;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/ws")
+public class RestApplication extends Application
+{
+}
